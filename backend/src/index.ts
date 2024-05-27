c1a2f12e4ed339d298127a0d1d03c9910117df62
hyperaid <abhinavsharma2k19@gmail.com>
2024-05-27 09:51:23 +0530

    integrated avien and cloudfare and login and signup routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,80 @@
 import { Hono } from 'hono'
+import { decode, jwt, sign, verify } from 'hono/jwt'
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono().basePath("/api/v1")
+const app=new Hono<{
+  Bindings:{
+    JWT_SECRET:string,
+    DATABASE_URL:String
+  }
+}>;
+app.post("/api/v1/signup", async (c) => {
+  // @ts-ignore
+  console.log(c.env.DATABASE_URL);
+    const prisma=new PrismaClient({
+  // @ts-ignore
 
-app.post("/signup", (c) => {
-  return c.text("a");
+      datasourceUrl:c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+    const body=await c.req.json();
+    const user=await prisma.user.create({
+      data:{
+        email:body.email,
+        password:body.password,
+        name:body.name,        
+
+      },
+    });
+      // const secret:string="abhinav";
+      const payload={ 
+        id:user.id,
+        // exp:Math.floor
+      }
+      //@ts-ignore
+      const token=await sign(payload,c.env.JWT_SECRET);
+      return c.json({
+        jwt:token
+      })
+
+
+
+
 });
-app.post("/signin",(c) =>{
+app.post("/api/v1/signin",async (c) =>{
+  const prisma=new PrismaClient({
+    // @ts-ignore
+  
+        datasourceUrl:c.env.DATABASE_URL
+      }).$extends(withAccelerate())
+      const dataa=await  c.req.json();
+      const user=await prisma.user.findUnique({
+        where:{
+          email:dataa.email,
+            
+        }
+
+      })
+
+      if(!user){
+        c.status(403)
+        return c.json({
+          error:"user not found"
+        })
+      }
+      //@ts-ignore
+      const jwt=await sign({id:user.id},c.env.JWT_SECRET)
+
+
   return c.text("dasnk");
 })
-app.post("/blog",(c) =>{
+app.post("/api/v1/blog",(c) =>{
   return c.text("dasnk");
 })
-app.put("/blog",(c) =>{
+app.put("/api/v1/blog",(c) =>{
   return c.text("dasnk");
 })
-app.get("/blog/:id",(c) =>{
+app.get("/api/v1/blog/:id",(c) =>{
   return c.text("dasnk");
 })
 
